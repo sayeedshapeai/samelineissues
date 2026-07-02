66f46015fe078f3a8b2241d162afe475e30d96b0
Sayeed <sayeedmohd1704@gmail.com>
2026-07-01 22:28:58 -0400

    app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import os windows
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "updated Flask sample application on azure hghapp service updated version-6"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 6000))
+    app.run(host="0.0.0.0", port=port, debug=False)
